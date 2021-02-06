62e537931c417519520b7d20d98ba992ea8a883d
lhrbuxiaoxin <lihaoran228@163.com>
2021-02-07 00:38:14 +0800

    添加项目文件。

---
+++ LabCMS.DocumentsDispatch.Shared/Models/IDocument.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabCMS.DocumentsDispatch.Shared.Models
+{
+    public interface IDocument
+    {
+        string Name { get; }
+        byte[] Content { get; }
+    }
+}
---
+++ LabCMS.DocumentsDispatch.Shared/Services/EmailSendService.cs
@@ -0,0 +1,19 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabCMS.DocumentsDispatch.Shared.Services
+{
+    public class EmailSendService
+    {
+        public void SendFile(string fileName,Stream stream)
+        {
+            
+        }
+    }
+}
